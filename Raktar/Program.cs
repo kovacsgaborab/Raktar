d3d959f107a7574312e8d549e3ea5809d628654b
kovacsgaborab <kovacsgaborab@gmail.com>
2020-09-23 14:57:24 +0200

    Házi beolvasni listaba

--- Raktar/Program.cs
+++ Raktar/Program.cs
@@ -3,16 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Raktar
 {
 
     class Program
     {
+
+        List<Termek> termekek = new List<Termek>();
+        static void BeolvasRaktar()
+        {
+            StreamReader raktar = new StreamReader("raktar.csv");
+
+            while (!raktar.EndOfStream)
+            {
+                //sor[0] - kód, sor[1] - nev ... stb.
+                string[] sor = raktar.ReadLine().Split(';');
+            }
+            raktar.Close();
+        }
+
         static void Main(string[] args)
         {
             Termek t = new Termek("Póló", "Póló XXL", 1500, 10);   //peldanyositas
 
+            BeolvasRaktar();
 
             Console.ReadKey();
         }
