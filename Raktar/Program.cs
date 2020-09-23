5575bc7213d81fd0aa3f1b4789f48475677af3af
kovacs gaborab <kovacsgaborab@gmail.com>
2020-09-23 21:18:26 +0200

    Valami gazvan

--- Raktar/Program.cs
+++ Raktar/Program.cs
@@ -11,7 +11,7 @@ namespace Raktar
     class Program
     {
 
-        List<Termek> termekek = new List<Termek>();
+        static List<Termek> termekek = new List<Termek>();
         static void BeolvasRaktar()
         {
             StreamReader raktar = new StreamReader("raktar.csv");
@@ -20,13 +20,19 @@ namespace Raktar
             {
                 //sor[0] - kód, sor[1] - nev ... stb.
                 string[] sor = raktar.ReadLine().Split(';');
+                Termek t = new Termek();
+                t.Kod = sor[0];
+                t.Nev = sor[1];
+                t.Ar = Convert.ToInt32(sor[2]);
+                t.Db =Convert.ToInt32(sor[3]);
+                termekek.Add(t);
             }
             raktar.Close();
         }
 
         static void Main(string[] args)
         {
-            Termek t = new Termek("Póló", "Póló XXL", 1500, 10);   //peldanyositas
+            /*Termek t = new Termek("Póló", "Póló XXL", 1500, 10);*/   //peldanyositas
 
             BeolvasRaktar();
 
